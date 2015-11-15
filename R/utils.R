3a605e2653db6cbc23042793848720b786b5cdcc
jaehyeon-kim <dottami@gmail.com>
2015-11-15 12:10:28 +1100

    basic utils updated

--- R/utils.R
+++ R/utils.R
@@ -5,3 +5,8 @@ get_dir_str <- function(dir_str = getwd()) {
     gsub('\\\\', '/', dir_str)
   }
 }
+
+set_file_path <- function(file_path = getwd(), file_name = NULL) {
+  full_path <- if(!is.null(file_name)) file.path(file_path, file_name) else file_path
+  get_dir_str(full_path)
+}
