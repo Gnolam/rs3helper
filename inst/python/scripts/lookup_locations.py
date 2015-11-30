36ddd302e2ed711c526b303dbd3e3679d97c1ea1
jaehyeon-kim <bernie.kim@anom.com.au>
2015-11-30 17:43:50 +1100

    old scripts deleted

--- inst/python/scripts/lookup_locations.py
+++
@@ -1,5 +0,0 @@
-import json
-
-from boto.s3.connection import Location
-
-print(json.dumps([l for l in dir(Location) if l[0].isupper()]))
--- inst/python/scripts/lookup_regions.py
+++
@@ -1,5 +0,0 @@
-import json
-
-import boto
-
-print(json.dumps([re.name for re in boto.s3.regions()]))
