e0bae5be6bd3050b1a218fd5cca767232c9ebc2d
jaehyeon-kim <bernie.kim@anom.com.au>
2015-12-02 17:14:05 +1100

    dependent packages import added

---
+++ R/rs3helper.R
@@ -0,0 +1,19 @@
+#' Python wrapper of boto s3 library
+#'
+#'
+#' \code{rs3helper} is a collection of functions on AWS S3.
+#' Each function is backed by a Python script that performs an action on the boto library - boto is probably the most popular interface to Amazon Web Services in Python.
+#' In this regards, this package can be considered as a Python wrapper of boto S3 library.
+#' For those who are interested in a native R package, see the aws.s3 package of the cloudyr project.
+#'
+#' Further details on boto and aws.s3 can be found below.
+#'
+#' An Introduction to boto S3 - \url{http://boto.cloudhackers.com/en/latest/s3_tut.html}
+#'
+#' AWS S3 Client Package - \url{https://github.com/cloudyr/aws.s3}
+#'
+#'
+#' @name rs3helper
+#' @import jsonlite
+#' @import pryr
+NULL
