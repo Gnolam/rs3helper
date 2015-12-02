4b3fe872ca8b665739d571ad93a65911ac8ca9a2
jaehyeon-kim <bernie.kim@anom.com.au>
2015-12-03 10:34:39 +1100

    vignette is being prepared

--- R/rs3helper.R
+++ R/rs3helper.R
@@ -1,8 +1,8 @@
-#' Python wrapper of boto s3 library
+#' R wrapper of Python boto s3 library
 #'
 #'
 #' \code{rs3helper} includes a collection of functions to work on AWS S3.
-#' Each function executes Python script that performs an action on the boto library - boto is probably the most popular interface to Amazon Web Services in Python.
+#' Each function executes a Python script that performs an action on the boto S3 library - boto is probably the most popular interface to Amazon Web Services in Python.
 #' In this regard, this package can be considered as a Python wrapper of boto S3 library.
 #' For those who are interested in a native R package, see the aws.s3 package of the cloudyr project.
 #'
